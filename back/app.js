67c2d83f9dd37847dc103f0dc78edf2ea5d48d82
Aayush Mishra <ayush950mishra@gmail.com>
2026-02-10 17:10:37 +0530

    Update app.js

--- back/app.js
+++ back/app.js
@@ -39,7 +39,7 @@
 // app.use(express.json());
 // app.use(
 //   cors({
-//     origin: "http://localhost:8080",
+//     origin: "https://emergency-frontend-clu9.onrender.com",
 //     credentials: true,
 //   })
 // );
